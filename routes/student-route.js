abc5418c3aba8486a4ae811c223c58a766683193
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-02 17:47:48 +0700

    Validasi tipe email dan delete

--- routes/student-route.js
+++ routes/student-route.js
@@ -10,4 +10,61 @@ router.get('/', (req, res) => {
 		.catch(err => res.send(err));
 })
 
+router.get('/add', (req, res) => {
+	res.render('add-student', {message: req.query.err});
+})
+
+router.post('/add', (req, res) => {
+	Model.Student.create(req.body, {
+			validate: {
+				 isEmail: {
+                    msg: "Email address must be valid"
+                }
+			}
+		})
+		.then(success => {
+			res.redirect('/students');
+		})
+		.catch(err => {
+			var msg = encodeURIComponent(err.message);
+  			res.redirect('/students/add/?err=' + msg);
+		});
+})
+
+router.get('/edit/:id', (req, res) => {
+	Model.Student.findById(req.params.id)
+		.then((studentEditFound) => {
+			res.render('add-student', {studentFound: studentEditFound, message: req.query.err});
+		})
+		.catch(err => res.send(err));
+})
+
+router.post('/edit/', (req, res) => {
+	Model.Student.update(req.body,{
+		where: {
+			id: req.body.id
+		},
+		validate: {
+			isEmail: {
+				msg: "Email address must be valid"
+			}
+		}
+	}).then(success =>{
+		res.redirect('/students');
+	}).catch(err => {
+		var msg = encodeURIComponent(err.message);
+		res.redirect(`/students/edit/${req.body.id}/?err=`+msg);
+	});
+})
+
+router.get('/delete/:id', (req, res) => {
+	Model.Student.destroy({
+		where: {
+			id: req.params.id
+		}
+	}).then(success =>{
+		res.redirect('/students');
+	}).catch(err => res.send(err));
+})
+
 module.exports = router;
