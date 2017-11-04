ccd9547cff9a2d8d981221a5799e5c70633d36db
ferdy <ferdy26790@gmail.com>
2017-11-05 01:50:01 +0700

    release11 error tampil pada halaman edt/add

--- router/student.js
+++ router/student.js
@@ -9,7 +9,7 @@ router.get('/students', (req,res)=> {
     studentsRows.forEach((student)=> {
       student.fullName = student.getFullName()
     })
-    res.render('student', {studentsRows,status})
+    res.render('student', {studentsRows, status})
   }).catch((err)=> {
     console.log(err)
   })
@@ -17,7 +17,8 @@ router.get('/students', (req,res)=> {
 //form tambah student
 router.get('/students/add', (req,res)=> {
   let status = 'add'
-  res.render('student', {status})
+  let msg = ""
+  res.render('student', {status, msg})
 })
 //menambah student
 router.post('/students/add', (req,res)=> {
@@ -27,21 +28,26 @@ router.post('/students/add', (req,res)=> {
   model.Students.create(obj).then((studentsRows)=> {
     res.redirect('/students')
   }).catch((err)=> {
-    console.log(err.errors[0].message)
+    //console.log(err.errors[0].message)
     if( err.errors[0].message == "Validation isEmail on email failed" ) {
-      res.send('email is not valid')
+      let status = 'add'
+      let msg = 'Email is not Valid'
+      res.render('student', {status, msg})
     }else if( err.errors[0].message == "email must be unique" ) {
-      res.send('email already taken')
+      let status = 'add'
+      let msg = 'Email is already taken'
+      res.render('student', {status, msg})
     }
   })
 })
 //form untuk edit student
 router.get('/students/edit/:id',(req,res)=> {
   let status = 'edit'
+  let msg = ""
   let id = req.params.id
   model.Students.findById(id).then((studentsRows)=>{
     //console.log(studentsRows.first_name)
-    res.render('student',{studentsRows,status})
+    res.render('student',{studentsRows, status, msg})
   }).catch((err)=>{
     console.log(err.errors[0].message)
   })
@@ -58,9 +64,19 @@ router.post('/students/edit/:id',(req,res)=>{
   }).catch((err)=>{
     console.log(err.errors[0].message)
     if( err.errors[0].message == "Validation isEmail on email failed" ) {
-      res.send('email is not valid')
+      let status = 'edit'
+      let msg = "Email is not valid"
+      model.Students.findById(idEdit).then((studentsRows)=>{
+        //console.log(studentsRows.first_name)
+        res.render('student',{studentsRows, status, msg})
+      })
     }else if( err.errors[0].message == "email must be unique" ) {
-      res.send('email already taken')
+      let status = 'edit'
+      let msg = "Email is already taken"
+      model.Students.findById(idEdit).then((studentsRows)=>{
+        //console.log(studentsRows.first_name)
+        res.render('student',{studentsRows, status, msg})
+      })
     }
   })
 })
