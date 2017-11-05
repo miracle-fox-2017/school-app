059dc39ef2d3c5d14a3bc32442e97cc6386547df
fujianto <septianahmad@naisinpo.com>
2017-11-05 18:05:02 +0700

    Sampai no 8 Score ok

--- routes/subject-route.js
+++ routes/subject-route.js
@@ -15,24 +15,44 @@ router.get('/', function (req, res) {
 router.get('/:id/enrolledstudents', function (req, res) {
 	Model.Subject.findById(req.params.id).then(foundSubject => {
 
-		// res.render('enrolled-student', {foundSubject: foundSubject});
-
 		Model.StudentSubject.findAll(
 		{
 			include: [Model.Student],
 			where: {
 				SubjectId: req.params.id
 			}
-		}
-		).then(allStudentSubjectData => {
-			// res.send(allStudentSubjectData);
-			// console.log(allStudentSubjectData);
+		}).then(allStudentSubjectData => {
 			res.render('enrolled-student', {foundSubject: foundSubject, students: allStudentSubjectData});
 		}).catch(err => res.send(err.message));
 
 	}).catch(err => res.send(err.message));
+})
 
+router.get('/:studentId/givescore', function (req, res) {
+	Model.StudentSubject.findOne({
+		include: [Model.Student, Model.Subject],
+		where: { id: req.params.studentId }
+	}).then(foundStudentSubject => {
+		res.render('give-score', { foundStudentSubject: foundStudentSubject})
 
+	}).catch(err => res.send(err));
+
+})
+
+router.post('/:id/givescore', function(req, res) {
+
+	Model.StudentSubject.update(
+		{
+			Score: req.body.Score
+		},
+		{
+			where: {
+				StudentId: req.body.StudentId
+			}
+		})
+		.then(allModelData => {
+			res.redirect('/subjects')
+		}).catch(err => res.send(err));
 })
 
 module.exports = router;
