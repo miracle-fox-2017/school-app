640bba07b1fd8aaa0da0906292873db1ecab493c
tomybudiman <tommybudi1998@gmail.com>
2017-11-05 19:28:41 +0700

    Amankan rilis 10

--- models/student.js
+++ models/student.js
@@ -3,12 +3,55 @@ module.exports = (sequelize, DataTypes) => {
   var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
+    email:{
+        type:DataTypes.STRING,
+        validate:{
+            isEmail:{
+                msg:"Masukan format email yang benar!"
+            },
+            isUnique:function(value,callback){
+                if(this.dataValues.id ===  null){ // Buat data baru
+                    Student.findOne({
+                        where:{
+                            email:this.dataValues.email
+                        }
+                    }).then((row)=>{
+                        if(row !== null){
+                            return callback("Email telah digunakan!");
+                        }
+                        return callback();
+                    }).catch((err)=>{
+                        return callback(err);
+                    });
+                }else{ // Update data lama
+                    Student.find({
+                        where:{
+                            email:this.dataValues.email
+                        }
+                    }).then((row)=>{
+                        if(row !== null && this.dataValues.id != row.dataValues.id){
+                            return callback("Email telah digunakan!");
+                        }else if(row !== null && this.dataValues.id == row.dataValues.id){
+                            return callback();
+                        }
+                        return callback();
+                    }).catch((err)=>{
+                        return callback(err);
+                    });
+                }
+            }
+        }
+    }
+  },{
+    classMethods:{
+        associate: function(models){
+            // associations can be defined here
+        }
+    },
+    instanceMethods:{
+        getFullName:function(first,last){
+            return first+" "+last;
+        }
     }
   });
   return Student;
--- router/students.js
+++ router/students.js
@@ -4,32 +4,58 @@ const router=express.Router();
 // Import Model
 const Model=require("../models");
 
+// Halaman awal students
 router.get("/",(req,res)=>{
-    Model.Student.findAll().then((rows)=>{
+    Model.Student.findAll({
+        order:[["id","ASC"]]
+    }).then((rows)=>{
         res.render("students",{studentRows:rows});
     }).catch((err)=>{
         res.send(err);
     });
 });
 
+// Halaman buat student
 router.get("/add",(req,res)=>{
-    res.render("student-add");
+    res.render("add-student",{error:""});
 });
 
+// Action buat student
 router.post("/add",(req,res)=>{
-    Model.Student.upsert(req.body).then((object)=>{
+    Model.Student.create(req.body).then((stats)=>{
         res.redirect("/students");
+    }).catch((err)=>{
+        res.render("add-student",{error:err.errors[0].message});
+    });
+});
+
+// Halaman edit student
+router.get("/edit/:id",(req,res)=>{
+    Model.Student.findById(req.params.id).then((rows)=>{
+        res.render("edit-student",{data:rows,error:""});
     }).catch((err)=>{
         res.send(err);
     });
 });
 
+// Action edit student
+router.post("/edit/:id",(req,res)=>{
+    Model.Student.update(req.body,{
+        where:{id:req.body.id}
+    }).then((stats)=>{
+        res.redirect("/students");
+    }).catch((err)=>{
+        Model.Student.findById(req.body.id).then((rows)=>{
+            res.render("edit-student",{data:rows,error:err.errors[0].message});
+        });
+    });
+});
+
+// Action hapus student
 router.get("/delete/:id",(req,res)=>{
     Model.Student.destroy({
-        where:{
-            id:req.params.id
-        }
-    }).then((object)=>{
+        where:{id:req.params.id}
+    }).then((stats)=>{
         res.redirect("/students");
     }).catch((err)=>{
         res.send(err);
