d45ae257ee0d321764755f53d8ece78030d12ad7
fujianto <septianahmad@naisinpo.com>
2017-11-06 13:06:39 +0700

    Fix bug conjunction give score

--- routes/subject-route.js
+++ routes/subject-route.js
@@ -13,30 +13,31 @@ router.get('/', function (req, res) {
 })
 
 router.get('/:subjectId/enrolledstudents', function (req, res) {
-	Model.Subject.findById(req.params.subjectId).then(foundSubject => {
-
-		Model.StudentSubject.findAll(
-		{
-			include: [{
-				model: Model.Student,
-			}],
-			order: [ [ { model: Model.Student, as: 'Student' }, 'first_name', 'ASC'] ],
-			where: {
-				SubjectId: req.params.subjectId
-			}
-		}).then(allStudentSubjectData => {
-			res.render('enrolled-student', {foundSubject: foundSubject, students: allStudentSubjectData});
-		}).catch(err => res.send(err.message));
-
-	}).catch(err => res.send(err.message));
+	Model.StudentSubject.findAll(
+	{
+		attributes: ['id', 'StudentId', 'SubjectId', 'Score'],
+		where: {
+			SubjectId: req.params.subjectId,
+		},
+		include: [{
+			model: Model.Student,
+		}],
+		order: [ [ { model: Model.Student, as: 'Student' }, 'first_name', 'ASC'] ]
+	}).then(allStudentSubjectData => {
+		res.render('enrolled-student', {students: allStudentSubjectData});
+	}).catch(err => res.send(err.message))
 })
 
-router.get('/:studentId/givescore', function (req, res) {
+router.get('/:subjectId/givescore', function (req, res) {
 	Model.StudentSubject.findOne({
 		attributes: ['id', 'StudentId', 'SubjectId', 'Score'],
-		include: [Model.Student, Model.Subject],
-		where: { StudentId: req.params.studentId }
+		where: { id: req.params.subjectId },
+		include: [
+			{ model: Model.Student},
+			{ model: Model.Subject}
+		]
 	}).then(foundStudentSubject => {
+		// res.send(foundStudentSubject);
 		res.render('give-score', { foundStudentSubject: foundStudentSubject})
 
 	}).catch(err => res.send(err));
@@ -51,6 +52,8 @@ router.post('/:id/givescore', function(req, res) {
 		{
 			where: {
 				id: req.body.StudentSubjectId,
+				StudentId: req.body.StudentId,
+				SubjectId: req.body.SubjectId
 			}
 		})
 		.then(allModelData => {
